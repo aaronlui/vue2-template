405d41cfe3705d847f4459d1339e91833b51953d
Aaron Lui <aaron_lui@163.com>
2024-11-07 11:02:11 +0800

    添加stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-standard-scss",
+    "stylelint-config-standard-vue/scss",
+    "stylelint-config-recess-order"
+  ],
+  "plugins": ["stylelint-order"],
+  "rules": {}
+}
