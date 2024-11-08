ed3bed797d03c72221df55ff5e60e2a40df78a79
Aaron Lui <aaron_lui@163.com>
2024-11-08 13:38:25 +0800

    弹框、分页二次封装

---
+++ src/components/FrDialog/index.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="fr-dialog" :class="{ 'has-footer': hasFooter }">
+    <el-dialog
+      :visible="value"
+      :title="title"
+      :width="width"
+      :fullscreen="fullscreen"
+      :top="top"
+      :modal="modal"
+      :modal-append-to-body="modalAppendToBody"
+      :append-to-body="appendToBody"
+      :lock-scroll="lockScroll"
+      :custom-class="customClass"
+      :close-on-click-modal="closeOnClickModal"
+      :close-on-press-escape="closeOnPressEscape"
+      :show-close="showClose"
+      :before-close="beforeClose"
+      :center="center"
+      :destroy-on-close="destroyOnClose"
+      @open="open"
+      @opened="opened"
+      @close="close"
+      @closed="closed"
+    >
+      <slot slot="title" name="title" />
+      <slot />
+      <slot slot="footer" name="footer" />
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import ElDragDialog from '@/directive/el-drag-dialog'
+export default {
+  directives: { ElDragDialog },
+  model: {
+    prop: 'value',
+    event: 'update:value'
+  },
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    width: {
+      type: String,
+      default: '50%'
+    },
+    fullscreen: {
+      type: Boolean,
+      default: false
+    },
+    top: {
+      type: String,
+      default: '0'
+    },
+    modal: {
+      type: Boolean,
+      default: true
+    },
+    modalAppendToBody: {
+      type: Boolean,
+      default: true
+    },
+    appendToBody: {
+      type: Boolean,
+      default: false
+    },
+    lockScroll: {
+      type: Boolean,
+      default: true
+    },
+    customClass: {
+      type: String,
+      default: ''
+    },
+    closeOnClickModal: {
+      type: Boolean,
+      default: false
+    },
+    closeOnPressEscape: {
+      type: Boolean,
+      default: true
+    },
+    showClose: {
+      type: Boolean,
+      default: true
+    },
+    beforeClose: {
+      type: Function,
+      default: done => done()
+    },
+    center: {
+      type: Boolean,
+      default: false
+    },
+    destroyOnClose: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    // 是否有footer
+    hasFooter() {
+      return !!this.$slots.footer
+    }
+  },
+  methods: {
+    open() {
+      this.$emit('update:value', true)
+
+      this.$emit('open')
+    },
+    opened() {
+      this.$emit('opened')
+    },
+    close() {
+      this.$emit('update:value', false)
+
+      this.$emit('close')
+    },
+    closed() {
+      this.$emit('closed')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.fr-dialog {
+
+}
+</style>
+
+<style lang="scss">
+@import "~@/styles/mixin";
+
+.fr-dialog {
+  .el-dialog {
+    top: 50%;
+    transform: translateY(-50%);
+
+    .el-dialog__body {
+      max-height: calc(90vh - 54px);
+      overflow-y: auto;
+
+      @include scrollbar;
+    }
+  }
+
+  &.has-footer {
+    .el-dialog__body {
+      max-height: calc(90vh - 54px - 70px);
+    }
+  }
+}
+</style>
---
+++ src/components/FrPagination/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="fr-pagination">
+    <el-pagination
+      :small="small"
+      :background="background"
+      :page-size="pageSize"
+      :total="total"
+      :pager-count="pagerCount"
+      :current-page="currentPage"
+      :layout="layout"
+      :page-sizes="pageSizes"
+      :popper-class="popperClass"
+      :prev-text="prevText"
+      :next-text="nextText"
+      :disabled="disabled"
+      :hide-on-single-page="hideOnSinglePage"
+      @size-change="sizeChange"
+      @current-change="currentChange"
+      @prev-click="prevClick"
+      @next-click="nextClick"
+    >
+      <slot />
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    small: {
+      type: Boolean,
+      default: false
+    },
+    background: {
+      type: Boolean,
+      default: true
+    },
+    pageSize: {
+      type: Number,
+      default: 10
+    },
+    total: {
+      type: Number,
+      default: 0
+    },
+    pagerCount: {
+      type: Number,
+      default: 7
+    },
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    layout: {
+      type: String,
+      default: 'total, sizes, prev, pager, next, jumper'
+    },
+    pageSizes: {
+      type: Array,
+      default() {
+        return [10, 20, 30, 40, 50, 100]
+      }
+    },
+    popperClass: {
+      type: String,
+      default: ''
+    },
+    prevText: {
+      type: String,
+      default: ''
+    },
+    nextText: {
+      type: String,
+      default: ''
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    hideOnSinglePage: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    sizeChange(size) {
+      this.$emit('update:pageSize', size)
+      this.$emit('update:currentPage', 1)
+
+      this.$emit('size-change', size)
+      this.$emit('change')
+    },
+    currentChange(page) {
+      this.$emit('update:currentPage', page)
+
+      this.$emit('current-change', page)
+      this.$emit('change')
+    },
+    prevClick(page) {
+      this.$emit('update:currentPage', page)
+
+      this.$emit('prev-click', page)
+      this.$emit('change')
+    },
+    nextClick(page) {
+      this.$emit('update:currentPage', page)
+
+      this.$emit('next-click', page)
+      this.$emit('change')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.fr-pagination {}
+</style>
+
+<style lang="scss">
+.fr-pagination {}
+</style>
